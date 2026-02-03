7a822557a2bc5a1d15a0e9408b82cb3736676d6a
cargoo <alejandrocamargo@Cargoos-Laptop.local>
2026-02-03 10:25:06 +0100

    done c++ comp

--- lsp/clangd.lua
+++ lsp/clangd.lua
@@ -1,5 +1,14 @@
 return {
   cmd = { 'clangd' },
+  init_options = {
+	  fallbackFlags = {
+		  "-std=c++98",
+		  "-Wextra",
+		  "-Werror",
+		  "-Wall"
+	  }
+
+  },
   root_markers = { '.clangd', 'compile_commands.json' },
   filetypes = { 'c', 'cpp' },
 }
